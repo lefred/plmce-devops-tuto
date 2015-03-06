ed6d47fed1739ca0f88fb4cb9d7bc8dbf2036cc2
frederic.descamps <frederic.descamps@percona.com>
2015-03-06 13:54:52 +0100

    support deb package name
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/server/packages.pp
+++ manifests/server/packages.pp
@@ -4,13 +4,15 @@ class mysql::server::packages {
       case $::osfamily {
           'RedHat': {
               $require = Yumrepo['mysql-repo']
+              $packs   = "Percona-Server-server-56" 
           }
           'Debian': {
               $require = Apt::Source['mysql-repo']
+              $packs   = "percona-server-server-56" 
           }
       }
       package {
-         "Percona-Server-server-56":
+         $packs:
             require => $require,
             ensure  => "installed";
       }
