df7916d70899e575f74d948eb3c69252bb93441b
frederic.descamps <frederic.descamps@percona.com>
2015-03-12 00:11:35 +0100

    add extra repo
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -6,25 +6,76 @@ class plmce::repository {
             'RedHat' : {
                $releasever = $operatingsystemmajrelease
                $basearch   = $hardwaremodel
+         
+           
+          if $plmce::mysqldistro == "community" {
+              $repo_descr = "MySQL $mysql::mysql_version Community Server"
+              $repo_url   = "http://repo.mysql.com/yum/mysql-${plmce::mysql_version}-community/el/$releasever/$basearch/"
+          } elsif $plmce::mysqldistro == "mariadb" {
+              $repo_descr = "MariaDB ${mysql_ver}"
+              if $basearch == "x86_64" {
+                  $mariaarch = "amd64"
+              } else {
+                  $mariaarch = "x86"
+              }
+              $repo_url   =  "http://yum.mariadb.org/${plmce::mysql_version}/centos${releasever}-${mariaarch}/"
+          } elsif $plmce::mysqldistro == "percona" {
+              $repo_descr = "Percona"
+              $repo_url   = "http://repo.percona.com/centos/$releasever/os/$basearch/"
+          } elsif $plmce::mysqldistro == "webscalesql" {
+              $repo_descr = "PSCE Repository"
+              $repo_url   = "http://repo.psce.com/psce/yum/$releasever/$basearch/" 
+          }
           
                yumrepo {
                   "mysql-repo":
-                     descr    => "PLMCE MySQL repo",
+                     descr    => $repo_descr,
                      enabled  => 1,
-                     baseurl  => "http://repo.percona.com/centos/$releasever/os/$basearch/",
+                     baseurl  => $repo_url,
                      gpgcheck => 0;
                }
             }
             'Debian': {
                include ::apt
+ 
+               if $plmce::mysql_distro == "community" {
+                 $repo_descr      = "MySQL $plmce::mysql_version Community Server"
+                 $repo_url        = "http://repo.mysql.com/apt/ubuntu/"
+                 $repo_repos      = "mysql-${plmce::mysql_version}"
+                 $repo_release    = $lsbdistcodename
+                 $repo_key        = "5072E1F5"
+                 $repo_key_source = false
+               } elsif $mysql::mysql_distro == "mariadb" {
+                 $repo_descr      = "MariaDB ${mysql_ver}"
+                 $repo_lsbdistid  = downcase($lsbdistid)
+                 $repo_url        =  "http://mariadb.cu.be//repo/${plmce::mysql_version}/${repo_lsbdistid}"
+                 $repo_repos      = "main"
+                 $repo_release    = $lsbdistcodename
+                 $repo_key        = "0xcbcb082a1bb943db"
+                 $repo_key_source = false
+               } elsif  $mysql::mysql_distro == "percona" {
+                 $repo_descr      = "Percona"
+                 $repo_url        = "http://repo.percona.com/apt/"
+                 $repo_repos      = "experimental"
+                 $repo_release    = $lsbdistcodename
+                 $repo_key        = "1C4CBDCDCD2EFD2A"
+                 $repo_key_source = "http://mirror.openminds.be/keys/1C4CBDCDCD2EFD2A.asc"
+               } elsif  $mysql::mysql_distro == "webscalesql" {
+                 $repo_descr      = "PSCE Repository"
+                 $repo_url        = "http://repo.psce.com/apt/"
+                 $repo_repos      = "main"
+                 $repo_release    = $lsbdistcodename
+               } 
                
+
+              
                apt::source {
                   "mysql-repo":
-                     location   => "http://repo.percona.com/apt/",
+                     location   => $repo_url,
                      release    => $lsbdistcodename,
-                     repos      => "main",
-                     key        => "1C4CBDCDCD2EFD2A",
-                     key_source => "http://mirror.openminds.be/keys/1C4CBDCDCD2EFD2A.asc",
+                     repos      => $repo_repos,
+                     key        => $repo_key,
+                     key_source => $repo_key_source,
                      include_src => false
               }
             }  
