72982645db37af8ea0565eeee1b877c095206f7b
frederic.descamps <frederic.descamps@percona.com>
2015-03-11 15:08:43 +0100

    meh
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,10 +2,9 @@ class plmce::server {
     include plmce::server::packages
     include plmce::server::service
     include plmce::server::config
-}
-
 class mysql::server inherits plmce::server {
-   
    info("in MySQL server class")
 }
+}
+
 
