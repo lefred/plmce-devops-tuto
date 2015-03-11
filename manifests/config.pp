9edf63668d91b48e063966080fe97fe77aefb016
frederic.descamps <frederic.descamps@percona.com>
2015-03-11 22:20:33 +0100

    add config for root password
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

---
+++ manifests/config.pp
@@ -0,0 +1,26 @@
+class plmce::config {
+   
+ $root_password = $plmce::mysql_root_password
+ $old_root_password = $plmce::mysql_old_root_password
+
+ if $root_password != undef {
+   case $old_root_password {
+     undef   : { $old_pwd = '' }
+     default : { $old_pwd = "-p'${old_root_password}'" }
+   }   
+ }
+
+ exec { 'set_root_pwd':
+     command   => "mysqladmin -u root ${old_pwd} password '${root_password}'",
+     logoutput => true,
+     unless    => "mysqladmin -u root -p'${root_password}' status > /dev/null",
+     path      => '/usr/local/sbin:/usr/bin:/usr/local/bin',
+ }
+ 
+ file { '/root/.my.cnf':
+     ensure  => present,
+     content => template("mysql/rootmy.cnf.erb"),
+     mode    => "0600",
+ }
+}
+ 
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class plmce ($mysqlserverid=undef) {
-               info("We load our MySQL module")
-               include plmce::repository
+      info("We load our MySQL module")
+      include plmce::repository
+      include plmce::config
 }       
 
