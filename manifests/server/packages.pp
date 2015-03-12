9af64e6985e1ac7930bd34e61086fb95686ce920
frederic.descamps <frederic.descamps@percona.com>
2015-03-12 23:49:28 +0100

    add packages
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/server/packages.pp
+++ manifests/server/packages.pp
@@ -1,14 +1,39 @@
 class plmce::server::packages {
+  
+      case $plmce::mysqldistro {
+        'community': {
+            $packs = ["mysql-community-server", 
+                      "mysql-community-client", 
+                      "mysql-community-libs-compat"]
+            $mysql_bin = "mysqld"
+        }
+        'mariadb': {
+            $packs = ["MariaDB-server",
+                      "MariaDB-client",
+                      "MariaDB-compat"]
+            $mysql_bin = "mysql"
+        }
+        'webscalesql': {
+            $packs = ["webscale-server", 
+                      "webscale-client"] 
+            $mysql_bin = "mysqld"
+        }
+        'percona': {
+            $packs = ["Percona-Server-server-${plmce::mysql_ver}", 
+                      "Percona-Server-client-${plmce::mysql_ver}"]
+            $mysql_bin = "mysql"
+        }
+
+
+      }
    
 
       case $::osfamily {
           'RedHat': {
               $require = Yumrepo['mysql-repo']
-              $packs   = "Percona-Server-server-56" 
           }
           'Debian': {
               $require = Apt::Source['mysql-repo']
-              $packs   = "percona-server-server-5.6" 
           }
       }
       package {
