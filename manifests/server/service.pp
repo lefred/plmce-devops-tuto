3e812a5b41e80b2cbba23ac386924bc0b78ef760
frederic.descamps <frederic.descamps@percona.com>
2015-03-13 00:07:38 +0100

    add name for service
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -3,6 +3,7 @@ class plmce::server::service {
     service {
         "mysql":
             enable  => true,
+            name    => $plmce::server::packages::mysql_bin,
             ensure  => running,
             require => Package[$plmce::server::packages::packs]
     }
