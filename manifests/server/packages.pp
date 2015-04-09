10004dc8d67e7f9340bf9f70b0c63ff8645fd4a7
Frederic Descamps <lefred.descamps@gmail.com>
2015-04-09 18:47:24 +0200

    try to fix mysql / mysqld issue
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/server/packages.pp
+++ manifests/server/packages.pp
@@ -4,26 +4,31 @@ class plmce::server::packages {
         'community': {
             $packs = ["mysql-community-server", 
                       "mysql-community-client", 
+            $mysql_bin = "mysqld"
                      ]
         }
         'mariadb': {
             $packs = ["MariaDB-server",
                       "MariaDB-client",
+            $mysql_bin = "mysql"
                      ]
         }
         'webscalesql': {
             $packs = ["webscalesql-server", 
                       "webscalesql-client"] 
+            $mysql_bin = "mysqld"
         }
         'percona': {
             case $::osfamily {
                'RedHat': {
                   $packs = ["Percona-Server-server-${plmce::mysql_ver}", 
                             "Percona-Server-client-${plmce::mysql_ver}"]
+                  $mysql_bin = "mysqld"
                }
                'Debian': {
                   $packs = ["percona-server-server-${plmce::mysqlversion}", 
                             "percona-server-client-${plmce::mysqlversion}"]
+                  $mysql_bin = "mysql"
                }
             }
         }
@@ -35,11 +40,9 @@ class plmce::server::packages {
       case $::osfamily {
           'RedHat': {
               $require = Yumrepo['mysql-repo']
-              $mysql_bin = "mysqld"
           }
           'Debian': {
               $require = Apt::Source['mysql-repo']
-              $mysql_bin = "mysql"
           }
       }
 
